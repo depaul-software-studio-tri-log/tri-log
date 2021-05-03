8fc8fd2c43c47f921c614a2d08b4a0beefbaae28
David Mathews <dmathew3@depaul.edu>
2021-05-02 20:11:34 -0500

    Added UserService and tri-log startup banner

--- src/main/java/com/depaul/trilog/web/HomeController.java
+++ src/main/java/com/depaul/trilog/web/HomeController.java
@@ -1,36 +1,20 @@
 package com.depaul.trilog.web;
 
-import com.depaul.trilog.dao.UserRepository;
-import com.depaul.trilog.entities.User;
+import com.depaul.trilog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.Authentication;
-
-import java.util.Optional;
-
 @Controller
 public class HomeController {
 
     @Autowired
-    private UserRepository userRepo;
+    private UserService userService;
 
     @GetMapping("/")
     public String home(Model model) {
-        // demo for getting the logged in user info
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String userName = authentication.getName();
-        Optional<User> user = userRepo.findByUsername(userName);
-        if (!user.isPresent()) {
-            // figure out what to do here
-        }
-        else {
-            User currentUser = user.get();
-            model.addAttribute("user", currentUser);
-        }
+        model.addAttribute("user", userService.getCurrentUser());
         return "home";
     }
 }
