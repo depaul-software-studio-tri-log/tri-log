69413200365d9df663d5c6d7223d11ae16e3194b
Kristie Athamanah <athamanah.kristie@gmail.com>
2021-05-10 15:28:03 -0500

    SE4-22 Add script to format birthdate for home page

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,12 +1,41 @@
-function calculateAge(birthdate) {
-    let date = new Date(birthdate);
+const birthdate = new Date(userBirthdate);
+
+function calculateAge() {
     let currentDate = new Date();
-    let age = currentDate.getUTCFullYear() - date.getUTCFullYear();
-    if (currentDate.getMonth() < date.getMonth()) {
+    let age = currentDate.getUTCFullYear() - birthdate.getUTCFullYear();
+
+    if (currentDate.getUTCMonth() < birthdate.getUTCMonth()) {
         age--;
     }
-    else if ((currentDate.getMonth() === date.getMonth()) && (currentDate.getUTCDate() < date.getUTCDate())) {
+    else if ((currentDate.getUTCMonth() === birthdate.getUTCMonth())
+        && (currentDate.getUTCDate() < birthdate.getUTCDate())) {
         age--;
     }
     document.getElementById("age").innerHTML = age;
 }
+
+function getOrdinalSuffix(date) {
+    let dateUTC = date.getUTCDate();
+    let ordinalSuffix = "th";
+
+    if ((dateUTC % 10 === 1) && (dateUTC !== 11)) {
+        ordinalSuffix = "st";
+    }
+    else if ((dateUTC % 10 === 2) && (dateUTC !== 12)) {
+        ordinalSuffix = "nd";
+    }
+    else if ((dateUTC % 10 === 3) && (dateUTC !== 13)) {
+        ordinalSuffix = "rd";
+    }
+    return ordinalSuffix;
+}
+
+function formatBirthday() {
+    let birthday = birthdate.toLocaleString('default',{month: 'long', day: 'numeric', timeZone: 'UTC'});
+    let ordinalSuffix = getOrdinalSuffix(birthdate);
+
+    document.getElementById("birthday").innerHTML = birthday + ordinalSuffix;
+}
+
+calculateAge();
+formatBirthday();
