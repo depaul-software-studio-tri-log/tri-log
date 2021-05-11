6c7f9b0f1afd053320c13ddfddce34d05c8e863d
Jose <josegacosta33@gmail.com>
2021-05-11 17:49:30 -0500

    reverting some files

--- src/database/schema/001-user.sql
+++ src/database/schema/001-user.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `user` (
-  `firstname` varchar(256) DEFAULT NULL,
-  `lastname` varchar(256) DEFAULT NULL,
+  `first_name` varchar(256) DEFAULT NULL,
+  `last_name` varchar(256) DEFAULT NULL,
   `email` varchar(256) NOT NULL,
-  `username` varchar(256) NOT NULL,
+  `user_name` varchar(256) NOT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   `password` varchar(256) DEFAULT NULL,
   `enabled` varchar(45) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `user_name_UNIQUE` (`user_name`),
   UNIQUE KEY `email_UNIQUE` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
