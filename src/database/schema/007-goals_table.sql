fa3ecafb09e85cd1ad3469388a786f1f0724ad3d
sla-m1 <steven.la@datastax.com>
2021-05-10 20:25:50 -0700

    Adding time, and changing goals create table to 007

--- src/database/schema/007-goals_table.sql
+++ src/database/schema/007-goals_table.sql
@@ -2,9 +2,10 @@ CREATE TABLE IF NOT EXISTS `goals` (
     `goalname` varchar(256) DEFAULT NULL,
     `activity` varchar(256) DEFAULT NULL,
     `distance` varchar(256) NOT NULL,
+    `minutes` varchar(256) DEFAULT NULL,
     `note` varchar(1024) DEFAULT NULL,
     `user_id` int NOT NULL,
     `id` int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`id`),
     FOREIGN KEY (user_id) REFERENCES user(id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
