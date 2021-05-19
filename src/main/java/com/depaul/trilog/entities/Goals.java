d8eed415bbc4e4f20c69bf83bebfdecc65ac24c4
sla7 <33646780+sla7@users.noreply.github.com>
2021-05-19 12:38:02 -0700

    Se4 31 goals submit (#13)
    * Created goal class, and setup Goals repository and service.
    
    * Added post function to form submit to save data into database. Need to figure out how to get current userid still.
    
    * Replacing static user_id with userService.getCurrentUser().getId().
    
    * Converting goals activity datatype to INT whhich will used to pull from the sport types table to set activity. Fixing bug where Thyleaf disabled automatic checking of a Sport when going to the goals page. Moving file structure to their designated folders and deleting the goals folder.
    
    * Adding in FOREIGN key for activity on goals table, and converted activity to INT allowing us to join the two tables to get sports type.
    
    * renaming sql file.
    
    Co-authored-by: sla-m1 <steven.la@datastax.com>

---
+++ src/main/java/com/depaul/trilog/entities/Goals.java
@@ -0,0 +1,81 @@
+package com.depaul.trilog.entities;
+
+import com.depaul.trilog.entities.SportType;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+public class Goals {
+    @Id
+    private int id;
+
+    @Column(name = "goalname")
+    private String goalname;
+    @Column(name = "activity")
+    private int activity;
+    @Column(name = "distance")
+    private int distance;
+    @Column(name = "minutes")
+    private int minutes;
+    @Column(name = "note")
+    private String note;
+    @Column(name = "user_id")
+    private int user_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGoalname() {
+        return goalname;
+    }
+
+    public void setGoalname(String goalname) {
+        this.goalname = goalname;
+    }
+
+    public int getActivity() {
+        return activity;
+    }
+
+    public void setActivity(int activity) {
+        this.activity = activity;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+}
--- src/main/java/com/depaul/trilog/web/GoalsController.java
+++ src/main/java/com/depaul/trilog/web/GoalsController.java
@@ -1,13 +1,31 @@
 package com.depaul.trilog.web;
 
+import com.depaul.trilog.dao.GoalsRepository;
+import com.depaul.trilog.entities.Goals;
+import com.depaul.trilog.services.GoalsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
 
 @Controller
 public class GoalsController {
+    @Autowired
+    private GoalsRepository goalRepo;
+
+    @Autowired
+    private GoalsService goalsService;
 
     @GetMapping("/goals")
-    public String goals() {
+    public String goals(Model model) {
+        model.addAttribute("goals", new Goals());
         return "goals";
+    }
+
+    @PostMapping("/addGoal")
+    public String createNewGoalSubmit(Goals goals) {
+        goalsService.addNewGoal(goals);
+        return "redirect:/goals";
     }
 }
