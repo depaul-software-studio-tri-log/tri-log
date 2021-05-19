8a43794fe7135e637755968205fc88aed8f5fd6a
Kristie Athamanah <athamanah.kristie@gmail.com>
2021-05-19 18:38:00 -0500

    Update plan controller to handle POST requests

--- src/main/java/com/depaul/trilog/web/PlanController.java
+++ src/main/java/com/depaul/trilog/web/PlanController.java
@@ -1,13 +1,31 @@
 package com.depaul.trilog.web;
 
+import com.depaul.trilog.entities.Plan;
+import com.depaul.trilog.services.PlanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PlanController {
 
+    @Autowired
+    private PlanService planService;
+
     @GetMapping("/plan")
-    public String plan() {
+    public String plan(Model model) {
+        model.addAttribute("plan", new Plan());
+        model.addAttribute("plans", planService.getAllPlans());
         return "plan";
+    }
+
+    @PostMapping("/plan/add")
+    public String save(@ModelAttribute("plan") Plan plan, BindingResult bindingResult, Model model) {
+        planService.savePlan(plan);
+        return "redirect:/plan";
     }
 }
