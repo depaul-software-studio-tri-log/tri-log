09b836abc4e88d863488c54300a4102ac0d36dcd
Alejandro Amaya <amayaalex19@gmail.com>
2021-05-19 17:31:25 -0500

    added page to input a run as well as all backend controller, entity, repo, and service

---
+++ src/database/schema/008-run.sql
@@ -0,0 +1,10 @@
+create table if not exists runs(
+runid INT NOT NULL AUTO_INCREMENT,
+distance INT,
+time  INT,
+rundate DATE,
+userid int,
+
+PRIMARY KEY(runid),
+Foreign Key(userid) references user(id)
+);
---
+++ src/main/java/com/depaul/trilog/services/RunService.java
@@ -0,0 +1,72 @@
+package com.depaul.trilog.services;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.depaul.trilog.dao.RunRepository;
+import com.depaul.trilog.entities.Run;
+import com.depaul.trilog.entities.User;
+
+@Service
+public class RunService {
+	
+	@Autowired
+	private RunRepository runRepo;
+	
+	
+	
+	public List<Run>getAllRuns(){
+		List <Run> runs = new ArrayList<>();
+		runRepo.findAll().forEach(run-> runs.add(run));
+		
+		return runs;
+		
+		
+	}
+	
+	public Run addRun(Run run) {
+		runRepo.save(run);
+		
+		return run;
+		
+		
+	}
+	
+	public void deleteRun(Run run) {
+		runRepo.delete(run);
+	}
+	
+	public List<Run> getRunByDate (Date runDate){
+		List <Run> runs = new ArrayList<Run>();
+		runRepo.findAll().forEach(date-> {
+			if(date.getRunDate().equals(runDate))
+				runs.add(date);
+		});
+		
+		return runs;
+		
+	}
+	
+	
+	
+	public Run getRun(Long runId) {
+		
+		 return runRepo.findById(runId).get();
+	}
+	
+	
+	public List<Run> getRunsByUser(User user){
+		List <Run> runs = new ArrayList<>();
+		runRepo.findByUser(user).forEach(runs::add);
+		return runs;
+	}
+	
+	
+	
+	
+
+}
