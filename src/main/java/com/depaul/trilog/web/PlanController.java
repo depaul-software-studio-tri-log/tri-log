a2d039bfd8eec93cfa211721ce517b5a0c8ffbda
Kristie Athamanah <athamanah.kristie@gmail.com>
2021-05-21 01:17:39 -0500

    Add preview section of saved plan data, update plan controller, and update input fields

--- src/main/java/com/depaul/trilog/web/PlanController.java
+++ src/main/java/com/depaul/trilog/web/PlanController.java
@@ -8,7 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 @Controller
 public class PlanController {
@@ -18,14 +23,29 @@ public class PlanController {
 
     @GetMapping("/plan")
     public String plan(Model model) {
-        model.addAttribute("plan", new Plan());
-        model.addAttribute("plans", planService.getAllPlans());
-        return "plan";
+        model.addAttribute("plans", planService.getAllPlansByUser());
+        return "plans/plan-overview";
+    }
+
+    @GetMapping("/plan/{date}")
+    public String planDay(@PathVariable(value="date") String date, Model model) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+
+        Plan plan = planService.findByDate(dateFormat.parse(date));
+        if (plan == null) {
+            plan = new Plan();
+            plan.setDate(dateFormat.parse(date));
+        }
+
+        model.addAttribute("plan", plan);
+        return "plans/plan-edit";
     }
 
     @PostMapping("/plan/add")
     public String save(@ModelAttribute("plan") Plan plan, BindingResult bindingResult, Model model) {
         planService.savePlan(plan);
-        return "redirect:/plan";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        String date = dateFormat.format(plan.getDate());
+        return "redirect:/plan/" + date;
     }
 }
