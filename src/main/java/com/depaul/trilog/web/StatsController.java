e05ac4304bba4874c1c7a2a5a174ab951398ba5f
sla-m1 <steven.la@datastax.com>
2021-05-25 11:25:26 -0700

    Starting point for cycling stats.

--- src/main/java/com/depaul/trilog/web/StatsController.java
+++ src/main/java/com/depaul/trilog/web/StatsController.java
@@ -1,10 +1,26 @@
 package com.depaul.trilog.web;
 
+import com.depaul.trilog.dao.CyclingRepository;
+import com.depaul.trilog.dao.RunRepository;
+import com.depaul.trilog.entities.Cycling;
+import com.depaul.trilog.entities.Goals;
+import com.depaul.trilog.services.CyclingService;
+import com.depaul.trilog.services.RunService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @Controller
 public class StatsController {
+
+    @Autowired
+    private CyclingRepository cyclingRepo;
+
+    @Autowired
+    private CyclingService cyclingServ;
 
     @GetMapping("/stats/overall")
     public String statsOverall() {
@@ -22,7 +38,18 @@ public class StatsController {
     }
 
     @GetMapping("/stats/cycling")
-    public String statsCycling() {
+    public String viewCycling(Model model) {
+        List<Cycling> cyclings = cyclingServ.getCyclingByUser();
+        List<String> datesdata = new ArrayList<>();
+        List<Integer> distances = new ArrayList<>();
+        cyclings.forEach(cycling-> {
+            datesdata.add(cycling.getCyclingDate().toString());
+            distances.add(cycling.getDistance());
+        });
+
+        model.addAttribute("cyclings", cyclingServ.getCyclingByUser());
+        model.addAttribute("dates", datesdata);
+        model.addAttribute("distances", distances);
         return "stats/cycling";
     }
 }
