8686291071d6fa0fbd7c0fce220e6b6d4f9649b6
David Mathews <dmathew3@depaul.edu>
2021-05-25 09:24:26 -0500

    added ability to delete races

--- src/main/java/com/depaul/trilog/web/RaceController.java
+++ src/main/java/com/depaul/trilog/web/RaceController.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class RaceController {
@@ -38,5 +41,18 @@ public class RaceController {
         raceRepository.save(race);
         return "redirect:/races";
     }
+
+    @GetMapping("/delete-race/{raceid}")
+    public String deleteRace(@PathVariable(value="raceid") int raceId, Model model) {
+        Optional<Race> race = raceRepository.findById(raceId);
+        if (race.isPresent()) {
+            Race existingRace = race.get();
+            if (existingRace.getUserid() == userService.getCurrentUser().getId()) {
+                raceRepository.delete(existingRace);
+            }
+        }
+        return "redirect:/races";
+    }
+
 
 }
