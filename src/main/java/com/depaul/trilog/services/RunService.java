ac76475d6bfd996aaca43c3ef0719bee33eefe9f
Jose <josegacosta33@gmail.com>
2021-05-26 15:09:15 -0500

    Added running stats page

--- src/main/java/com/depaul/trilog/services/RunService.java
+++ src/main/java/com/depaul/trilog/services/RunService.java
@@ -13,60 +13,60 @@ import com.depaul.trilog.entities.User;
 
 @Service
 public class RunService {
-	
+
 	@Autowired
 	private RunRepository runRepo;
-	
-	
-	
+
+
+
 	public List<Run>getAllRuns(){
 		List <Run> runs = new ArrayList<>();
 		runRepo.findAll().forEach(run-> runs.add(run));
-		
+
 		return runs;
-		
-		
+
+
 	}
-	
+
 	public Run addRun(Run run) {
 		runRepo.save(run);
-		
+
 		return run;
-		
-		
+
+
 	}
-	
+
 	public void deleteRun(Run run) {
 		runRepo.delete(run);
 	}
-	
+
 	public List<Run> getRunByDate (Date runDate){
 		List <Run> runs = new ArrayList<Run>();
 		runRepo.findAll().forEach(date-> {
 			if(date.getRunDate().equals(runDate))
 				runs.add(date);
 		});
-		
+
 		return runs;
-		
+
 	}
-	
-	
-	
+
+
+
 	public Run getRun(Long runId) {
-		
-		 return runRepo.findById(runId).get();
+
+		return runRepo.findById(runId).get();
 	}
-	
-	
+
+
 	public List<Run> getRunsByUser(User user){
 		List <Run> runs = new ArrayList<>();
 		runRepo.findByUser(user).forEach(runs::add);
 		return runs;
 	}
-	
-	
-	
-	
+
+
+
+
 
 }
