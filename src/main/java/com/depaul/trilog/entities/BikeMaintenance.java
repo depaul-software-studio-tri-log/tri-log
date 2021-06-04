2a39f85887f091bdbbc94568d85cea8f7540f201
Alejandro Amaya <amayaalex19@gmail.com>
2021-06-03 20:07:51 -0500

    fixed bikemaintenance entity

--- src/main/java/com/depaul/trilog/entities/BikeMaintenance.java
+++ src/main/java/com/depaul/trilog/entities/BikeMaintenance.java
@@ -1,9 +1,13 @@
 package com.depaul.trilog.entities;
 
+import java.io.Serializable;
 import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -14,9 +18,11 @@ import lombok.Data;
 @Data
 @Table(name = "bike_maintenance")
 @Entity
-public class BikeMaintenance {
+public class BikeMaintenance implements Serializable {
+    private static final long serialVersionUID = 3L;
 
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "maintenanceID")
 	private int maintenanceID;
 	
