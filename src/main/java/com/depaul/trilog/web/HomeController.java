fe6cf4a704918f517dcacdd000e417cd682e2ecb
Kristie Athamanah <athamanah.kristie@gmail.com>
2021-06-04 10:51:58 -0500

    Display today's training plan on home page based on data from database

--- src/main/java/com/depaul/trilog/web/HomeController.java
+++ src/main/java/com/depaul/trilog/web/HomeController.java
@@ -1,18 +1,25 @@
 package com.depaul.trilog.web;
 
 import com.depaul.trilog.dao.RaceRepository;
+import com.depaul.trilog.entities.Plan;
 import com.depaul.trilog.entities.User;
+import com.depaul.trilog.services.PlanService;
 import com.depaul.trilog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Date;
+
 @Controller
 public class HomeController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PlanService planService;
 
     @Autowired
     private RaceRepository raceRepository;
@@ -22,6 +29,10 @@ public class HomeController {
         User user = userService.getCurrentUser();
         model.addAttribute("user", user);
         model.addAttribute("nextRace", raceRepository.getNextRaceForUser(user.getId()));
+
+        Date dateToday = new Date();
+        Plan planToday = planService.findByDate(dateToday);
+        model.addAttribute("planToday", planToday);
         return "home";
     }
 }
