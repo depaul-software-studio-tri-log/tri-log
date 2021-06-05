1f5eaf1304ce8f4834524bb9a8a0bd172fb118a1
Kristie Athamanah <athamanah.kristie@gmail.com>
2021-06-05 00:41:02 -0500

    Add methods to goals service and repository to get a goal or lists of goals

--- src/main/java/com/depaul/trilog/dao/GoalsRepository.java
+++ src/main/java/com/depaul/trilog/dao/GoalsRepository.java
@@ -4,8 +4,11 @@ import com.depaul.trilog.entities.Goals;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 @Repository
 public interface GoalsRepository extends JpaRepository<Goals, Integer> {
-//    List<Goal> findByUser_id(int User_id);
+    List<Goals> findGoalsByUseridOrderByIdDesc(int userid);
+    List<Goals> findGoalsByActivity(int activity);
+    Goals findFirstById(int id);
 }
