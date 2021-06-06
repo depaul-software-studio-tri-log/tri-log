ab321fb98e1f76e38125c8af420c155a24359b78
David Mathews <dmathew3@depaul.edu>
2021-06-06 18:37:17 -0500

    first pass at password reset - it creates a token but does not send the email yet

--- src/main/java/com/depaul/trilog/services/UserService.java
+++ src/main/java/com/depaul/trilog/services/UserService.java
@@ -2,23 +2,62 @@ package com.depaul.trilog.services;
 
 import com.depaul.trilog.dao.UserRepository;
 import com.depaul.trilog.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.core.env.Environment;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     private UserRepository userRepo;
+
+    @Autowired
+    private Environment env;
 
     public User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String userName = authentication.getName();
         Optional<User> user = userRepo.findByUsername(userName);
         return user.get();
+    }
+
+    public void sendPasswordResetEmailIfUserExists(String email) {
+        Optional<User> user = userRepo.findByEmail(email);
+        if (user.isPresent()) {
+            String token = UUID.randomUUID().toString();
+            User targetUser = user.get();
+            targetUser.setPasswordresettoken(token);
+            Date expirationDate = getTokenExpirationDate();
+            targetUser.setPasswordresetexpires(expirationDate);
+            userRepo.save(targetUser);
+            sendPasswordResetEmail(user);
+        }
+        else {
+            logger.info("No user found for email " + email);
+        }
+    }
+
+    private void sendPasswordResetEmail(User user) {
+
+        logger.info("Sent password reset email to user " + user.getId());
+    }
+
+    private Date getTokenExpirationDate() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(calendar.getTime());
+        calendar.add(Calendar.HOUR_OF_DAY, Integer.parseInt(env.getProperty("passwordExpirationLifetimeInHours")));
+        return calendar.getTime();
     }
 }
--- src/main/java/com/depaul/trilog/web/LoginController.java
+++ src/main/java/com/depaul/trilog/web/LoginController.java
@@ -1,12 +1,19 @@
 package com.depaul.trilog.web;
 
+import com.depaul.trilog.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    UserService userService;
 
     @GetMapping("/login")
     public String login(@RequestParam(value = "error", required = false) String error,
@@ -22,5 +29,22 @@ public class LoginController {
     @GetMapping("/create")
     public String createAccount() {
         return "create-account";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword(@RequestParam(value = "confirmation", required = false) String error,
+                                 Model model) {
+        String confirmationMessage = null;
+        if(error != null) {
+            confirmationMessage = "Submission successful";
+        }
+        model.addAttribute("confirmationMessage", confirmationMessage);
+        return "forgot-password";
+    }
+
+    @PostMapping("/request-password-reset")
+    public String requestPasswordReset(String email) {
+        userService.sendPasswordResetEmailIfUserExists(email);
+        return "redirect:/forgot-password?confirmation=true";
     }
 }
